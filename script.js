b20aa09ae596816bab35fea15ac6149d41dfaa49
sharifa-bala <sharifa.bala@gmail.com>
2025-05-21 20:53:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    // Example: smooth scroll for anchor links
+    $("a").on("click", function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $("html, body").animate({ scrollTop: $(hash).offset().top }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
